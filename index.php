462233ac25af80609a36e6e1ca57cbd1f8dbd0a0
Hoàng Thanh Hà <49011681+hoangha090399@users.noreply.github.com>
2020-04-23 10:16:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
 </head>
 <body>
 
-  <h1>Cloud-Php-Ex000</h1>
+  <h1>Loai tam giac</h1>
   <p>Hello my name is Ha</p>
-
+  <form action="action.php" method="get">
+   Do dai 1: <input type="text" name="a"><br/>
+   Do dai 2: <input type="text" name="b"><br/>
+   Do dai 3: <input type="text" name="c"><br/>
+   <input type="submit" value="Do">
+</form>
 </body>
 </html>
